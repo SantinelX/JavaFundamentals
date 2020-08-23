fa22a2560db8fea17ff4f68530ed1dadafc2afe1
SantinelX <balazs_calin@yahoo.com>
2020-08-23 09:55:30 +0300

    ceva

---
+++ SDASchool.java
@@ -0,0 +1,21 @@
+public class SDASchool {
+
+    public static void main(String[] args) {
+
+        // TemaAdoua.exercitiul6();
+        // TemaTrei.exercitiu1();
+        // TemaTrei.exercitiu2();
+        // TemaTrei.exercitiu3();
+        // TemaTrei.exercitiu4();
+      //TemaTrei.exercitiu5();
+        // TemaTrei.exercitiul5v2();
+       TemaAdoua.exercitiul5();
+
+    }
+}
+
+
+
+
+
+
